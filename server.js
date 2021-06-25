4267932df510a91410b9e20ea8d796ae9ff01952
heinnssin <heinnssinp@gmail.com>
2021-06-25 18:06:18 -0400

    backend-running

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express();
-const {syncAndSeed, db, models: {Dealer, Car, Client} } = require('./db')
+const {syncAndSeed, db, models: {Dealer, Car, Client} } = require('./db.js')
 const PORT = 3000
 
 
-app.get('/api/cars', async(req, res, next)=>{
+const DIST_PATH = path.join(__dirname, './dist');
+
+app.use(express.static(DIST_PATH));
+app.use(express.json());
+
+
+app.get('/cars', async(req, res, next)=>{
   try {
     const response = await Car.findAll()
     res.send(response)
@@ -14,7 +21,7 @@ app.get('/api/cars', async(req, res, next)=>{
   }
 })
 
-app.get('/api/dealers', async(req, res, next)=>{
+app.get('/dealers', async(req, res, next)=>{
   try {
     const response = await Dealer.findAll()
     res.send(response)
@@ -24,7 +31,7 @@ app.get('/api/dealers', async(req, res, next)=>{
   }
 })
 
-app.get('/api/clients', async(req, res, next)=>{
+app.get('/clients', async(req, res, next)=>{
   try {
     const response = await Client.findAll()
     res.send(response)
